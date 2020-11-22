cdac9e1b538040f3805836dea4a5ff9bfa2b324e
jayasuhane1979-you <74330647+jayasuhane1979-you@users.noreply.github.com>
2020-11-22 07:50:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+
+var mathoper=require('./mathoper.js');
+console.log("Additon of two numbers:", mathoper.AddNumber(1,2));
+console.log("Substraction of two numbers:", mathoper.SubNumber(20,10));
+console.log("Multiplication of two numbers:",mathoper.MulNumber(3,4));
+console.log("Division of two numbers:",mathoper.DivNumber(4,2));
---
+++ mathoper.js
@@ -0,0 +1,17 @@
+var exports=module.exports={};
+exports.AddNumber=function(a,b)
+{
+	return a+b;
+};
+exports.SubNumber=function(a,b)
+{
+	return a-b;
+};
+exports.MulNumber=function(a,b)
+{
+	return a*b;
+};
+exports.DivNumber=function(a,b)
+{
+	return a/b;
+};
